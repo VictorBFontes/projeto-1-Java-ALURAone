9bdc64e5971758a78c8a98cd3f7fdcbad0b15e8f
VictorBFontes <victor.jose.barros.fontes@gmail.com>
2024-05-23 11:35:39 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exemplo {
+
+    public void main(String[] args) {
+        System.out.println("Oi, meu nome é Alura!");
+    }
+
+}
